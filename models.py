dab4db23ef1ae9645ef1269555b03e77632c5165
My Name <my-name@chromium.org>
2023-06-07 22:46:08 +0300

    events bug fixed

--- models.py
+++ models.py
@@ -164,12 +164,12 @@ class Conv1D(nn.Module):
         self.xshape2 = xshape2
         self.calculate_output_length(length_in=xshape1, kernel_size=2, stride=1, padding=0, dilation=1)
         # todo this is not correct!
-        self.conv1d = nn.Conv1d(xshape1, l1, kernel_size=2 )
+        self.conv1d = nn.Conv1d(xshape1, xshape2, kernel_size=2 )
         self.max_pooling = nn.MaxPool1d(kernel_size=2)
         self.flatten = nn.Flatten()
-        self.dense1 = nn.Linear(l1*((self.xshape2-1)//2), l2)
-        self.dense2 = nn.Linear(l2, l3)
-        self.dense3 = nn.Linear(l3, 2)
+        self.dense1 = nn.Linear(self.xshape1 *self.xshape2 * 10, l1)
+        self.dense2 = nn.Linear(l1, l2)
+        self.dense3 = nn.Linear(l2, l3)
         self.dropout = nn.Dropout(p=args.dropout)
         self.activation = self.args.activation
         self.sigmoid = nn.Sigmoid()
